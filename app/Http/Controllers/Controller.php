2e042af9593f4db7a09742b9ed0291228ef17c58
= <=>
2022-11-18 12:22:42 -0500

    ok goude

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Http\Request;
+use App\Models\Client;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
@@ -16,23 +17,25 @@ class Controller extends BaseController
         return view('connecter'); 
     }
 
-    /*
     public function authentifier(Request $request){
         $request->validate([
-            'courriel' => ['required', 'string', 'min:5', 'max:35' ],    
+            'courriel' => ['required', 'string', 'min:5', 'max:35' ] ,   
             'motDePasse' => ['required', 'string',  'min:5', 'max:35']
         ]);
 
         $courriel = $request->input('courriel');
         $motDePasse = $request->input('motDePasse');
 
-        if(Auth::attempt(['courriel' => $courriel, 'motDePasse' => $motDePasse])){
+        $user = Client::find($courriel, $motDePasse);
+
+        dd ($user);
+        /*
             return redirect()->route('accueil');
         }else{
             return redirect()->route('connecter');
         }
+        */
     }
-    */
     public function apropos(){
 
     }
