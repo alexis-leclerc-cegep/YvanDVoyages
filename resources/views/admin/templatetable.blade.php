dc5226bdbb8964226e8e354881abf96427aaad2a
= <=>
2022-12-01 16:20:13 -0500

    ok goude

---
+++ resources/views/admin/templatetable.blade.php
@@ -0,0 +1,12 @@
+
+<!doctype html>
+<html lang="fr">
+    <head>
+        <script src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-dark-5@1.1.3/dist/css/bootstrap-dark.min.css">
+        <livewire:styles />
+    </head>
+<body>
+    <livewire:scripts />
+    @yield('contenu')
+</body>
