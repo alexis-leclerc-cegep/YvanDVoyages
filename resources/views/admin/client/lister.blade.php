32efdfa3853a12d8734ea94a4cceecc382f7a49d
= <=>
2022-12-01 15:49:24 -0500

    ok goude

--- resources/views/admin/client/lister.blade.php
+++ resources/views/admin/client/lister.blade.php
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
 @extends('./admin/templatetable')
 @section('contenu')
 <livewire:clients-table theme="bootstrap-4"/>
 @endsection
-=======
-@extends('./client/template')
-@section('contenu')
-    <h2>ok</h2>
-    <livewire:table :config="App\Tables\ClientsTable::class" />
-
-@endsection
->>>>>>> 2539db1dfb95ee67c2f1e5ed42d4a10f1afa8301
