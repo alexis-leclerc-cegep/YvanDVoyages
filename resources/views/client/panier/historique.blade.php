124b1397c0f2d4bf236f7a0a12acf3239f9d1949
alexis-leclerc-cegep <1942518@cegeprdl.ca>
2022-12-06 21:33:06 -0500

    ok goude

--- resources/views/client/panier/historique.blade.php
+++ resources/views/client/panier/historique.blade.php
@@ -5,6 +5,7 @@
 @section('contenu')
 <script src="{{ asset('js/jquery.js') }}"></script>
 @php
+	use Carbon\Carbon;
 	$listeVentes = $client->vente;
 @endphp
 <div class="archive post-type-archive post-type-archive-product woocommerce woocommerce-page">
@@ -16,7 +17,7 @@
 				<main id="main" class="site-main" role="main">
 				<ul class="products">
 					@if($listeVentes->count() > 0)
-					    @foreach ($listeVentes as $itemVente) 
+					    @foreach ($listeVentes->reverse() as $itemVente) 
 						    <li style="width: 90%; margin-bottom:24px; border:2px black solid;">
 							<a href="../voyage/detailler/{{$itemVente->voyage->id }}" style="display:flex; flex-direction:row; margin-bottom:10px">
 								<div>
@@ -25,15 +26,13 @@
 								<div style="margin-left: 25%">
 									<h3>{{$itemVente->voyage->nomVoyage}}</h3><br>
 									<span class="price"><span class="amount">${{ $itemVente->voyage->prix }} par participant</span></span><br>
+									<span class="price"><span class="amount">Voyage commandé le {{Carbon::parse($itemVente->dateVente)->toFormattedDateString()}}</span></span><br>
+									<span class="price"><span class="amount">Nombre de voyageurs : {{$itemVente->quantiteVoyageurs}}</span></span><br>
 								</div>
 
 							</a>
-							<div style="margin-left:10px">
-								<span>Quantité : {{$itemVente->quantiteVoyageurs}}
-							</div>
 							<br>
 							<!--<a href="./panier/modifier/Quantite/{{ $itemVente->id }}/-1" type="button" class="btn btn-success" style="width: 30px;">-</a>-->
-							<a href="./supprimer/{{$itemVente->id}}" class="button" style="margin-bottom:10px; margin-top:15px; margin-left:10px">Supprimer</a>
 							</li>
 						@endforeach
 						<style>
@@ -41,24 +40,6 @@
 								padding-left: 70px !important;
 							}
 						</style>
-						<table style="width:fit-content">
-							<tr>
-								<th>Sous-total</th> 
-								<td>{{ number_format($client->totalPanier(), 2) }}$</td>
-							</tr>
-							<tr>
-								<th>Montant taxes</th>
-								<td>{{ number_format($client->totalPanier() * 0.15, 2) }}$</td>
-							</tr>
-							<tr>
-								<th>Total</th>
-								<td>{{ number_format($client->totalPanier() * 1.15, 2) }}$</td>
-							</tr>
-							<br>
-							<br>
-							<span id="avertissement" style="display: none" class="danger">Rafraîchir pour avoir le montant de la commande actualisé</span>
-						</table>
-						<a href="./paiement" class="button" >Passer la commande</a>
 					@endif
 						
 
